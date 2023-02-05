f8e92bf17c43aa9e24dca0be55f4fe0e014ee2dc
yodabitch <87611453+Ranavicky@users.noreply.github.com>
2023-02-05 17:17:15 +0530

    final

--- js/scripts.js
+++ js/scripts.js
@@ -98,3 +98,28 @@
     });
 
 })(jQuery);
+
+
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("msg").value,
+  };
+
+  const serviceID = "service_85egqvv";
+  const templateID = "template_74r2acw";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
